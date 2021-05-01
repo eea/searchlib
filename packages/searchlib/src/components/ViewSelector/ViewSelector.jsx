578fd44706e595aa4ea261ae2f9e7f0dc5a619b9
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-05-01 21:58:24 +0300

    Enable table listing button

--- packages/searchlib/src/components/ViewSelector/ViewSelector.jsx
+++ packages/searchlib/src/components/ViewSelector/ViewSelector.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 const ViewSelector = (props) => {
-  return <div>Views</div>;
+  const { views, active, onSetView } = props;
+  return (
+    <Button.Group compact>
+      {views.map((view) => {
+        const { render: Render, icon: Icon, title } = view;
+        return Render ? (
+          <Render {...props} />
+        ) : (
+          <Button
+            active={view.id === active}
+            onClick={() => onSetView(view.id)}
+          >
+            {Icon ? <Icon {...props} /> : title}
+          </Button>
+        );
+      })}
+    </Button.Group>
+  );
 };
 
 export default ViewSelector;
