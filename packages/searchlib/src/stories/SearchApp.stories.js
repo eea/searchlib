ba9a67a843285729e056dee83646715c3434edb9
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-05-03 09:48:53 +0300

    Fix storybook

--- packages/searchlib/src/stories/SearchApp.stories.js
+++ packages/searchlib/src/stories/SearchApp.stories.js
@@ -1,5 +1,9 @@
 import SearchApp from '@eeacms/search/SearchApp';
+import config from '@eeacms/search/registry';
+import installDemo from '@eeacms/search/demo';
+
 import '@eeacms/search/index.css';
+import '@eeacms/search/semantic-ui.less';
 
 const page = {
   title: 'Search/Demo',
@@ -18,7 +22,10 @@ const page = {
 };
 export default page;
 
-const Template = (args) => <SearchApp {...args} />;
+const Template = (args) => {
+  const registry = installDemo(config);
+  return <SearchApp registry={registry} {...args} />;
+};
 
 export const WiseDemo = Template.bind({});
 // WiseDemo.args = {
