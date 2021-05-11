507c49c8970906844e623e2ac083d1e6ddb1696e
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-05-11 06:35:30 +0300

    Fix eslint configuration

--- packages/demo/src/index.js
+++ packages/demo/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import '@eeacms/search/index.css';
 import { SearchApp } from '@eeacms/search';
 
-// import 'semantic-ui-css/semantic.min.css';
 import registry from '@eeacms/search/registry';
 import installDemo from './demo';
 
+// import '@eeacms/search/index.css';
+// import 'semantic-ui-css/semantic.min.css';
 import './semantic-ui.less';
 
 const demoRegistry = installDemo(registry);
