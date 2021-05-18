07ee120e0937e0202adba6935aaf4ce86f7d200e
miahi <mihai.ivascu@eaudeweb.ro>
2021-05-18 07:29:04 -0400

    Added sample dags

--- airflow/README.md
+++ airflow/README.md
@@ -4,7 +4,6 @@ For development (easier access to dag files) use the following:
 
 ```
 cp docker-compose.override.yaml.dev docker-compose.override.yaml
-mkdir dags
 mkdir logs
 mkdir plugins
 chown 50000:50000 dags logs plugins
---
+++ airflow/dags/searchengine/crawler/crawl_start.py
@@ -0,0 +1,37 @@
+from datetime import timedelta
+from textwrap import dedent
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['me@miahi.ro'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+with DAG(
+    'crawl_start',
+    default_args=default_args,
+    description='Scheduled crawl startup',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(2),
+    tags=['miahi'],
+) as dag:
+
+    read_site_list = BashOperator(
+        task_id='read_site_list',
+        bash_command='date',
+    )
+
+    iterate_start = BashOperator(
+        task_id='iterate_start',
+        depends_on_past=False,
+        bash_command='sleep 5',
+        retries=3,
+    )
+
+    read_site_list >> [iterate_start]
