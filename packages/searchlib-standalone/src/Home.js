cf29d39d531b7d384b04dece0f9987f7082626cf
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-05-19 08:24:03 +0300

    More package structure

--- packages/searchlib-standalone/src/Home.js
+++ packages/searchlib-standalone/src/Home.js
@@ -1,31 +1,15 @@
 import React from 'react';
-import logo from './react.svg';
-import './Home.css';
+import installDemo from './config';
+
+import '@elastic/react-search-ui-views/lib/styles/styles.css';
+
+const demoRegistry = installDemo(registry);
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="Home">
-        <div className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <h2>Welcome to Razzle</h2>
-        </div>
-        <p className="Home-intro">
-          To get started, edit <code>src/App.js</code> or{' '}
-          <code>src/Home.js</code> and save to reload.
-        </p>
-        <ul className="Home-resources">
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-          </li>
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-          </li>
-          <li>
-            <a href="https://palmer.chat">Community Slack</a>
-          </li>
-        </ul>
-      </div>
+      __SERVER__ ? '' :
+      <SearchApp registry={demoRegistry} appName="wise" />
     );
   }
 }
