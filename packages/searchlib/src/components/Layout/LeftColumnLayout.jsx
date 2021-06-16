a931ed757dedb44b6e0f183e1afa579629afa13e
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-06-16 15:13:43 +0300

    Use SUI for leftcolumnlayout

--- packages/searchlib/src/components/Layout/LeftColumnLayout.jsx
+++ packages/searchlib/src/components/Layout/LeftColumnLayout.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
-import { Layout as SUILayout } from '@elastic/react-search-ui-views'; // SingleSelectFacet
+import { Grid, Container } from 'semantic-ui-react';
 
-const LeftColumnLayout = (props) => <SUILayout {...props} />;
+const LeftColumnLayout = (props) => {
+  const { bodyContent, bodyFooter, bodyHeader, header, sideContent } = props;
+  return (
+    <div>
+      <Container>
+        <div className="sui-layout-header">
+          <div className="sui-layout-header__inner">{header}</div>
+        </div>
+      </Container>
+
+      <Grid columns={2} container stackable className="body-content">
+        <Grid.Row>
+          <Grid.Column widescreen="4">
+            <div>{sideContent}</div>
+          </Grid.Column>
+          <Grid.Column widescreen="12">
+            <div>{bodyHeader}</div>
+            <div>{bodyContent}</div>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Grid container className="body-footer">
+        <div>{bodyFooter}</div>
+      </Grid>
+    </div>
+  );
+};
 
 export default LeftColumnLayout;
