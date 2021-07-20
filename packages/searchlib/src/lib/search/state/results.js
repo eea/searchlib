43af924c2f974f32e89e1d6dc84c87d0408aa2fa
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-07-20 14:04:32 +0300

    Add compatibility with output from haystack

--- packages/searchlib/src/lib/search/state/results.js
+++ packages/searchlib/src/lib/search/state/results.js
@@ -22,18 +22,24 @@ export default function buildResults(hits, field_filters) {
   });
 
   const toObject = (field, value, snippet) => {
-    const bl = field_filters[field]?.blacklist || [];
-    const wl = field_filters[field]?.whitelist || [];
+    const blacklist = field_filters[field]?.blacklist || [];
+    const whitelist = field_filters[field]?.whitelist || [];
+
     if (!Array.isArray(value)) {
       value = [value];
     }
-    let filtered_value = value.filter((val) => bl.indexOf(val) === -1);
-    if (wl.length > 0) {
-      filtered_value = filtered_value.filter((val) => wl.indexOf(val) !== -1);
+
+    let filtered_value = value.filter((val) => blacklist.indexOf(val) === -1);
+    if (whitelist.length > 0) {
+      filtered_value = filtered_value.filter(
+        (val) => whitelist.indexOf(val) !== -1,
+      );
     }
+
     if (filtered_value.length === 1) {
       filtered_value = filtered_value[0];
     }
+
     return { raw: filtered_value, ...(snippet && { snippet }) };
   };
 
@@ -45,9 +51,14 @@ export default function buildResults(hits, field_filters) {
       ])
       .reduce(addEachKeyValueToObject, {});
 
-    if (!Object.keys(rec).includes('id')) {
-      rec.id = { raw: record._id }; // TODO: make sure to have ids
+    rec.id = { raw: record._id || record.id }; // TODO: make sure to have ids
+
+    if (rec.source) {
+      // compatibility with haystack proxy
+      rec._source = rec.source;
+      delete rec._source;
     }
+
     rec._original = record;
     return rec;
   });
