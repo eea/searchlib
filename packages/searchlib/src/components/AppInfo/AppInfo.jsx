7acc6d9a54f9d28c70967490ef459faebfaac00c
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-08-02 22:06:28 +0300

    Add wise search engine

--- packages/searchlib/src/components/AppInfo/AppInfo.jsx
+++ packages/searchlib/src/components/AppInfo/AppInfo.jsx
@@ -4,17 +4,22 @@ import { DateTime } from 'luxon';
 
 //async function getInfo(appConfig) {
 const getInfo = async (appConfig) => {
+  const { elastic_index } = appConfig;
+
+  if (elastic_index === '_all') return ''; // we don't support _all
+
   const info = await getIndexInfo(appConfig);
-  const index = Object.keys(info);
-  if (!index.length) return '';
-  const creation_ts = info[index].settings.index.creation_date;
+  const indexes = Object.keys(info);
+  if (!indexes.length || indexes.indexOf(elastic_index) === -1) return '';
+
+  const creation_ts = info[elastic_index].settings.index.creation_date;
   const dt = DateTime.fromMillis(parseInt(creation_ts));
   return dt.toLocaleString(DateTime.DATETIME_FULL);
 };
 
 function AppInfo({ appConfig, ...rest }) {
   const { app_name, app_version } = appConfig;
-  const hostname = window.runtimeConfig.HOSTNAME;
+  const hostname = window.runtimeConfig?.HOSTNAME || 'localhost';
   const [crt, setCrt] = React.useState([]);
 
   React.useEffect(() => {
