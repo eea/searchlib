c64ff33548cb72af9c7b91747a52d6065400e243
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-08-12 12:46:26 +0300

    Make package build

--- packages/searchlib-middleware/src/server.js
+++ packages/searchlib-middleware/src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createESMiddleware } from '@eeacms/search-middleware';
+import { createESMiddleware } from './elasticsearch';
 
 // import installConfig from './config';
 // import { registry } from '@eeacms/search';
