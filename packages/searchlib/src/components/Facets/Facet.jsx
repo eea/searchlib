27a168e7a5089e451aa79dc0715c6554ffef1d79
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-08-23 15:52:28 +0300

    Allow passing values in simple facet

--- packages/searchlib/src/components/Facets/Facet.jsx
+++ packages/searchlib/src/components/Facets/Facet.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { withSearch, Facet as SUIFacet } from '@elastic/react-search-ui';
 import { Accordion, Icon } from 'semantic-ui-react';
+import MultiCheckboxFacet from './MultiCheckboxFacet';
 
 const FacetComponent = (props) => {
-  // console.log('sui props', props);
   const { collapsable = true, filters = [], field, label } = props;
   const hasFilter = !!filters.find((filter) => field === filter.field);
   const [isOpened, setIsOpened] = React.useState(hasFilter);
-
   return collapsable ? (
     <Accordion>
       <Accordion.Title active={isOpened} onClick={() => setIsOpened(!isOpened)}>
@@ -15,7 +14,7 @@ const FacetComponent = (props) => {
         {label}
       </Accordion.Title>
       <Accordion.Content active={isOpened}>
-        <SUIFacet {...props} active={isOpened} />
+        <SUIFacet {...props} active={isOpened} view={MultiCheckboxFacet} />
       </Accordion.Content>
     </Accordion>
   ) : (
