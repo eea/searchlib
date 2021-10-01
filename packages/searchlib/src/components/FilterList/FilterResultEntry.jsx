09cd0c62e329d309ad16c71f79f49989f2f6f7e5
GhitaB <ghitabizau27@gmail.com>
2021-10-01 17:02:33 +0300

    Refs #139198 - More like this: prepare info for card.

--- packages/searchlib/src/components/FilterList/FilterResultEntry.jsx
+++ packages/searchlib/src/components/FilterList/FilterResultEntry.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import { Icon } from 'semantic-ui-react';
 import { useAtom } from 'jotai';
 import { moreLikeThisAtom } from '@eeacms/search/state';
+import { DateTime, StringList } from '@eeacms/search/components';
 
 const FilterResultEntry = (props) => {
   const { value } = props;
   const [result] = useAtom(moreLikeThisAtom);
 
-  console.log('filter result', result);
-
   if (result) {
-    const itemURL = result.about.raw;
     return (
-      <a href={itemURL} target="_blank" rel="noreferrer">
-        <Icon name="external" size="small" />
-        {itemURL}
-      </a>
+      <div class="mlt-card">
+        <a href={result.about.raw} target="_blank" rel="noreferrer">
+          <Icon name="external" size="small" />
+          {result.title.raw}
+        </a>
+        <p>
+          <DateTime format="DATE_MED" value={result.issued.raw} />
+          &nbsp;|&nbsp;
+          <StringList value={result.subject.raw} />
+        </p>
+        <p>{result.description.raw}</p>
+      </div>
     );
   }
   return value;
