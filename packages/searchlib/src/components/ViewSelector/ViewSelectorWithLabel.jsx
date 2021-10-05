2eeb203ca10735dd073bef5faf52cff47773b9e1
GhitaB <ghitabizau27@gmail.com>
2021-10-05 15:56:04 +0300

    Refs #139198 - WIP dropdown.

--- packages/searchlib/src/components/ViewSelector/ViewSelectorWithLabel.jsx
+++ packages/searchlib/src/components/ViewSelector/ViewSelectorWithLabel.jsx
@@ -1,11 +1,57 @@
 import React from 'react';
 import { Button, Container, Icon } from 'semantic-ui-react';
+import { Dropdown } from 'semantic-ui-react';
 
 // TODO: refactor as dropdown
 const ViewSelectorWithLabel = (props) => {
   const { views, active, onSetView } = props;
+
+  const friendOptions = [
+    {
+      key: 'Jenny Hess',
+      text: 'Jenny Hess',
+      value: 'Jenny Hess',
+      image: { avatar: true, src: '/images/avatar/small/jenny.jpg' },
+    },
+    {
+      key: 'Elliot Fu',
+      text: 'Elliot Fu',
+      value: 'Elliot Fu',
+      image: { avatar: true, src: '/images/avatar/small/elliot.jpg' },
+    },
+    {
+      key: 'Stevie Feliciano',
+      text: 'Stevie Feliciano',
+      value: 'Stevie Feliciano',
+      image: { avatar: true, src: '/images/avatar/small/stevie.jpg' },
+    },
+    {
+      key: 'Christian',
+      text: 'Christian',
+      value: 'Christian',
+      image: { avatar: true, src: '/images/avatar/small/christian.jpg' },
+    },
+  ];
+
+  console.log(views);
+  const tempOptions = views.map((view) => ({
+    key: view.id,
+    text: view.title,
+    value: view.title,
+  }));
+  console.log(tempOptions);
+
   return (
     <Container className="view-selector">
+      <span>
+        Display as{' '}
+        <Dropdown
+          inline
+          options={tempOptions}
+          defaultValue={tempOptions[0].value}
+        />
+      </span>
+      {/*
       Display as
       <Button.Group size="tiny" compact>
         {views.map((view) => {
@@ -22,7 +68,7 @@ const ViewSelectorWithLabel = (props) => {
             </Button>
           );
         })}
-      </Button.Group>
+      </Button.Group>*/}
     </Container>
   );
 };
