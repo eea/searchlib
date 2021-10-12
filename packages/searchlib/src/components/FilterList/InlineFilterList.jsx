d088334db73c3f54cc36ca7d7a737011d49af843
GhitaB <ghitabizau27@gmail.com>
2021-10-12 11:36:17 +0300

    Refs #139198 - Fix bug: keep show filters btn visible when no filters applied.

--- packages/searchlib/src/components/FilterList/InlineFilterList.jsx
+++ packages/searchlib/src/components/FilterList/InlineFilterList.jsx
@@ -11,7 +11,7 @@ const InlineFilterList = (props) => {
   const [isOpened, setIsOpened] = React.useState(false);
   const [showFacets, setShowFacets] = useAtom(showFacetsAsideAtom);
 
-  return filters.length ? (
+  return (
     <div className="inline-filter-list">
       <Button
         className="show-filters"
@@ -24,68 +24,70 @@ const InlineFilterList = (props) => {
         <Icon name="filter" />
         {showFacets ? 'Hide filters' : 'Show filters'}
       </Button>
-      <Segment inverted className="filter-list">
-        <Accordion inverted>
-          <Accordion.Title
-            className="filter-list-header"
-            active={isOpened}
-            onClick={() => setIsOpened(!isOpened)}
-          >
-            <div>
-              <Icon name="dropdown" />
-              Current filters applied: {filters.length}
-            </div>
-            <Button
-              className="reset-filters"
-              compact
-              basic
-              inverted
-              size="mini"
-              onClick={(e) => {
-                e.stopPropagation();
-                e.preventDefault();
-                clearFilters();
-                Object.keys(defaultFilters).map((filter, index) => {
-                  setFilter(
-                    filter,
-                    defaultFilters[filter]?.value,
-                    defaultFilters[filter]?.type,
-                  );
-                  return true;
-                });
-              }}
+      {filters.length ? (
+        <Segment inverted className="filter-list">
+          <Accordion inverted>
+            <Accordion.Title
+              className="filter-list-header"
+              active={isOpened}
+              onClick={() => setIsOpened(!isOpened)}
             >
-              Reset
-              <Icon name="undo alternate" />
-            </Button>
-          </Accordion.Title>
-          <Accordion.Content className="filter-list-content" active={isOpened}>
-            <Divider inverted />
-            <div className="filter">
-              {filters.map((filter, index) => {
-                return (
-                  <Filter
-                    key={index}
-                    {...filter}
-                    setFilter={setFilter}
-                    removeFilter={removeFilter}
-                    onClear={(field) => {
-                      const activeFilters = filters.map(({ field }) => field);
-                      const exclude = activeFilters.filter(
-                        (name) => name !== field,
-                      );
-                      clearFilters(exclude);
-                    }}
-                  />
-                );
-              })}
-              <div className="filter-list-footer"></div>
-            </div>
-          </Accordion.Content>
-        </Accordion>
-      </Segment>
+              <div>
+                <Icon name="dropdown" />
+                Current filters applied: {filters.length}
+              </div>
+              <Button
+                className="reset-filters"
+                compact
+                basic
+                inverted
+                size="mini"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  e.preventDefault();
+                  clearFilters();
+                  Object.keys(defaultFilters).map((filter, index) => {
+                    setFilter(
+                      filter,
+                      defaultFilters[filter]?.value,
+                      defaultFilters[filter]?.type,
+                    );
+                    return true;
+                  });
+                }}
+              >
+                Reset
+                <Icon name="undo alternate" />
+              </Button>
+            </Accordion.Title>
+            <Accordion.Content className="filter-list-content" active={isOpened}>
+              <Divider inverted />
+              <div className="filter">
+                {filters.map((filter, index) => {
+                  return (
+                    <Filter
+                      key={index}
+                      {...filter}
+                      setFilter={setFilter}
+                      removeFilter={removeFilter}
+                      onClear={(field) => {
+                        const activeFilters = filters.map(({ field }) => field);
+                        const exclude = activeFilters.filter(
+                          (name) => name !== field,
+                        );
+                        clearFilters(exclude);
+                      }}
+                    />
+                  );
+                })}
+                <div className="filter-list-footer"></div>
+              </div>
+            </Accordion.Content>
+          </Accordion>
+        </Segment>
+      ) : null}
     </div>
-  ) : null;
+  );
 };
 
 export default InlineFilterList;
