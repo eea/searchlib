9422d9e049919614c6540b29dec5a5141f842f05
GhitaB <ghitabizau27@gmail.com>
2021-10-14 10:57:36 +0300

    Refs #140559 - Update button text: Show more filters.

--- packages/searchlib-globalsearch/src/global-search-config.js
+++ packages/searchlib-globalsearch/src/global-search-config.js
@@ -23,7 +23,7 @@ const globalSearchConfig = {
   title: 'Global search and catalogue',
   layoutComponent: 'FilterAsideLayout',
   contentBodyComponent: 'FilterAsideContentView',
-  enableNLP: true, // enables NLP capabilities
+  enableNLP: false, // enables NLP capabilities
   facetsListComponent: 'VerticalCardsModalFacets',
 
   extraQueryParams: {
