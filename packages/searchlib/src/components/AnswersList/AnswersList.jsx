b104aa7427cb7c233667f7a085629e7359f06db5
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-10-19 15:49:41 +0300

    Add loader to answerbox

--- packages/searchlib/src/components/AnswersList/AnswersList.jsx
+++ packages/searchlib/src/components/AnswersList/AnswersList.jsx
@@ -1,30 +1,22 @@
 import React from 'react';
-import cx from 'classnames';
 
-import { Segment, Label, Rating, Popup, Button } from 'semantic-ui-react'; //, Icon, Accordion
+import {
+  Segment,
+  Rating,
+  Popup,
+  Button,
+  Dimmer,
+  Loader,
+} from 'semantic-ui-react'; //, Icon, Accordion
 
 import { ExternalLink } from '@eeacms/search/components/Result/HorizontalCardItem';
 import { convertHitToResult } from '@eeacms/search/lib/search/state/results';
 import { useAppConfig } from '@eeacms/search/lib/hocs';
-import { DateTime } from '@eeacms/search/components'; //, StringList
 import AnswerBoxDetails from './AnswerBoxDetails';
+import { highlightUrl } from './utils';
+import AnswerLinksList from './AnswersLinksList';
 
 import withAnswers from './withAnswers';
-
-const highlightUrl = (url, text) => {
-  return `${url}#:~:text=${encodeURIComponent(text)}`;
-  // TODO: ideally we'd use this library, but it is too much tied up to DOM
-  // https://github.com/GoogleChromeLabs/text-fragments-polyfill/blob/main/src/fragment-generation-utils.js
-  // const start = text.slice(0, 8);
-  // const end = text.slice(text.length - 8, text.length);
-  // return `${url}#:~:text=${encodeURIComponent(start)},${encodeURIComponent(
-  //   end,
-  // )}`;
-};
-
-const extractDomain = (url) => {
-  return url ? new URL(url).hostname : url;
-};
 
 const AnswerContext = ({ item, href }) => {
   const { full_context, answer } = item;
@@ -55,7 +47,7 @@ const AnswerContext = ({ item, href }) => {
 const AnswersList = (props) => {
   const { appConfig } = useAppConfig();
   const { horizontalCardViewParams } = appConfig;
-  const { titleField, urlField } = horizontalCardViewParams;
+  const { urlField } = horizontalCardViewParams;
   const { answers = [], loading, loaded, searchedTerm } = props;
   const { searchContext } = props;
   const { searchTerm = '' } = searchContext;
@@ -93,90 +85,45 @@ score: 6.118757247924805
   return (
     <div className="answers-list">
       {showLoader ? (
-        ''
-      ) : searchTerm && searchedTerm === searchTerm && filtered?.length ? (
-        <Segment className="answers-wrapper">
-          <div className="answerCard">
-            <h3 className="answers__directAnswer">{filtered[0].answer}</h3>
-            <AnswerContext
-              item={primaryAnswer}
-              href={primaryResult[urlField]?.raw}
-            />
-            <div className="answers__links">
-              {filtered.slice(0, 5).map((item, i) => {
-                const result = convertHitToResult(
-                  { ...item, _source: item.source },
-                  appConfig.field_filters,
-                );
-                const date = Date.parse(result['issued']?.raw);
-                const days =
-                  result && (Date.now() - date) / 1000 / 60 / 60 / 24;
-                let expired =
-                  result?.['expires']?.raw !== undefined
-                    ? Date.parse(result['expires']?.raw) < Date.now()
-                    : false;
-
-                return (
-                  <div key={i} className={cx({ primary: i === 0 })}>
-                    <span className="answer__date">
-                      <DateTime
-                        format="DATE_MED"
-                        value={result['issued']?.raw}
-                      />
-                    </span>
-                    <ExternalLink
-                      href={highlightUrl(result[urlField]?.raw, item.answer)}
-                    >
-                      {result[titleField]?.raw}
-                    </ExternalLink>
-                    <span className="answer__domain">
-                      Source:{' '}
-                      <ExternalLink
-                        href={highlightUrl(result[urlField]?.raw, item.answer)}
-                      >
-                        {extractDomain(result[urlField]?.raw)}
-                      </ExternalLink>
-                    </span>
-                    {days < 30 && (
-                      <>
-                        &nbsp;
-                        <Label className="new-item" horizontal>
-                          New
-                        </Label>
-                      </>
-                    )}
-                    {expired && (
-                      <>
-                        &nbsp;
-                        <Label className="archived-item" horizontal>
-                          Archived
-                        </Label>
-                      </>
-                    )}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-          <div className="answers__bottom">
-            <Rating
-              rating={Math.round(5 * primaryAnswer.score)}
-              maxRating={5}
-              size="mini"
-              disabled
-            />
-            <div className="answers__bottom__spacer"></div>
-            <Popup
-              trigger={
-                <Button basic size="mini">
-                  Direct answer
-                </Button>
-              }
-            >
-              <AnswerBoxDetails />
-            </Popup>
+        <Segment className="answers__loading">
+          <div className="loading-tip">Searching for direct answers...</div>
+          <div className="progress">
+            <div className="color"></div>
           </div>
         </Segment>
+      ) : searchTerm && searchedTerm === searchTerm && filtered?.length ? (
+        <>
+          <Segment className="answers-wrapper">
+            <div className="answerCard">
+              <h3 className="answers__directAnswer">{filtered[0].answer}</h3>
+              <AnswerContext
+                item={primaryAnswer}
+                href={primaryResult[urlField]?.raw}
+              />
+              <div className="answers__links">
+                <AnswerLinksList appConfig={appConfig} filtered={filtered} />
+              </div>
+            </div>
+            <div className="answers__bottom">
+              <Rating
+                rating={Math.round(5 * primaryAnswer.score)}
+                maxRating={5}
+                size="mini"
+                disabled
+              />
+              <div className="answers__bottom__spacer"></div>
+              <Popup
+                trigger={
+                  <Button basic size="mini">
+                    Direct answer
+                  </Button>
+                }
+              >
+                <AnswerBoxDetails />
+              </Popup>
+            </div>
+          </Segment>
+        </>
       ) : (
         ''
       )}
