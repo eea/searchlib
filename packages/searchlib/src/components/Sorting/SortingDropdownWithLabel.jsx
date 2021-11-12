e38463af4d830f8a3f48182b837b5b2694e404ff
GhitaB <ghitabizau27@gmail.com>
2021-11-12 15:33:01 +0200

    Refs #140518 - Sort by - improve.

--- packages/searchlib/src/components/Sorting/SortingDropdownWithLabel.jsx
+++ packages/searchlib/src/components/Sorting/SortingDropdownWithLabel.jsx
@@ -7,14 +7,24 @@ const SortingViewComponent = (props) => {
   const { label, options, onChange } = props;
   const searchContext = useSearchContext();
   const { sortField, sortDirection } = searchContext;
+  const sortOptions = options.map(({ label, value }) => {
+    return { text: label, value };
+  });
+
+  const activeValue = `${sortField}|||${sortDirection}`;
+
+  const activeLabel = sortOptions.filter(({ text, value }) => {
+    return value === activeValue;
+  })[0].text;
+
   return (
     <div className="sorting">
       <span>
-        {label}
         <Dropdown
+          text={`Sort by ${activeLabel}`}
           inline
-          value={`${sortField}|||${sortDirection}`}
-          options={options.map(({ label, value }) => ({ text: label, value }))}
+          value={activeValue}
+          options={sortOptions}
           onChange={(e, { value }) => {
             onChange(value);
           }}
