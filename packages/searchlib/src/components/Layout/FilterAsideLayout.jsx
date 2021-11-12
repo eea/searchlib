4121dad386d47b74df8bc7bd0ce8776608beab31
GhitaB <ghitabizau27@gmail.com>
2021-11-12 11:08:55 +0200

    Refs #140518 - Add class names for answers section.

--- packages/searchlib/src/components/Layout/FilterAsideLayout.jsx
+++ packages/searchlib/src/components/Layout/FilterAsideLayout.jsx
@@ -21,10 +21,10 @@ const FilterAsideLayout = (props) => {
         <div className="sui-layout-header">
           <div className="sui-layout-header__inner">{header}</div>
 
-          <Grid columns={2} container stackable>
+          <Grid columns={2} container stackable className="answers-section">
             <Grid.Row>
               <Grid.Column widescreen="2"></Grid.Column>
-              <Grid.Column widescreen="8">
+              <Grid.Column widescreen="8" className="answers-content">
                 <AnswersList />
               </Grid.Column>
             </Grid.Row>
