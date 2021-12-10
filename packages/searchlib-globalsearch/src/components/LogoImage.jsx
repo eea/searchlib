a84921b4f3e0501d150c2fac44ffe1f0a5e52259
GhitaB <ghitabizau27@gmail.com>
2021-12-10 15:08:21 +0200

    Refs #142953 - Website icons.

--- packages/searchlib-globalsearch/src/components/LogoImage.jsx
+++ packages/searchlib-globalsearch/src/components/LogoImage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
+import { useAppConfig } from '@eeacms/search/lib/hocs';
 
 const LogoImage = (props) => {
-  const { image } = props;
+  const { websiteName } = props;
+  const { appConfig } = useAppConfig();
+  const icons = appConfig.contentUtilsParams.iconsDicts['Sources'];
+  const logo = icons[websiteName] || icons.fallback;
 
   return (
-    <Image className="facet-option-icon" src={require('./logos/' + image)} />
+    <Image className="facet-option-icon" src={require('./logos/' + logo)} />
   );
 };
 
--- packages/searchlib-globalsearch/src/components/WebsiteFilterListComponent.jsx
+++ packages/searchlib-globalsearch/src/components/WebsiteFilterListComponent.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import LogoImage from './LogoImage';
 
 const WebsiteFilterListComponent = (props) => {
   const { value } = props;
   // console.log('props', props);
-  return <span>{value}</span>;
+  return (
+    <span>
+      <LogoImage websiteName={value} />
+      {value}
+    </span>
+  );
 };
 
 export default WebsiteFilterListComponent;
