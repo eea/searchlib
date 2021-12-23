f0ff2bd38a4e31cc2fd4963d91b293ce4b9d3450
GhitaB <ghitabizau27@gmail.com>
2021-12-23 16:33:26 +0200

    Refs #136090 - Add back and next buttons.

--- packages/searchlib/src/components/PagingInfo/PagingPrevNext.jsx
+++ packages/searchlib/src/components/PagingInfo/PagingPrevNext.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Button, Icon } from 'semantic-ui-react';
 
 import cx from 'classnames';
 
@@ -15,14 +16,30 @@ function PagingPrevNext({
   return (
     <div className={cx('sui-paging-info', className)} {...rest}>
       {start > 1 ? (
-        <Button className="prev" compact color="green" size="mini">
+        <Button
+          className="prev"
+          compact
+          color="green"
+          size="mini"
+          as={Link}
+          to="/prev"
+        >
+          <Icon name="angle double left" />
           back
         </Button>
       ) : null}
       Results {start} - {end} of {totalResults}{' '}
       {end < totalResults ? (
-        <Button className="next" compact color="green" size="mini">
+        <Button
+          className="next"
+          compact
+          color="green"
+          size="mini"
+          as={Link}
+          to="/next"
+        >
           next
+          <Icon name="angle double right" />
         </Button>
       ) : null}
     </div>
