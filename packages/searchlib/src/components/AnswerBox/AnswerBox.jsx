0303ce726356e77674025d64e797bdd63d1d7b34
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-01-06 14:55:37 +0200

    Styling tweaks to answer box

--- packages/searchlib/src/components/AnswerBox/AnswerBox.jsx
+++ packages/searchlib/src/components/AnswerBox/AnswerBox.jsx
@@ -46,6 +46,18 @@ const AnswerContext = ({ item, answerItem }) => {
       <span dangerouslySetInnerHTML={{ __html: pre }}></span>
       <ExternalLink href={highlightUrl(item.href, ans)}>
         <span dangerouslySetInnerHTML={{ __html: ans }}></span>
+        <Popup
+          trigger={
+            <Rating
+              rating={Math.round(5 * answerItem.score)}
+              maxRating={5}
+              size="mini"
+              disabled
+            />
+          }
+        >
+          <AnswerBoxDetails />
+        </Popup>
       </ExternalLink>{' '}
       <span dangerouslySetInnerHTML={{ __html: post }}></span> (
       <DateTime format="DATE_MED" value={item.issued} />)
@@ -133,6 +145,9 @@ score: 6.118757247924805
                     appConfig,
                   )
                 : null;
+              const links = filtered
+                .slice(1, filtered.length)
+                .slice(1, Math.min(filtered.length, MAX_COUNT));
 
               return (
                 <div
@@ -146,32 +161,16 @@ score: 6.118757247924805
                         item={primaryResult}
                         answerItem={primaryAnswer}
                       />
-                      <div className="answers__links">
-                        <AnswerLinksList
-                          appConfig={appConfig}
-                          filtered={filtered
-                            .slice(1, filtered.length)
-                            .slice(1, Math.min(filtered.length, MAX_COUNT))}
-                        />
-                      </div>
-                    </div>
-                    <div className="answers__bottom">
-                      <Rating
-                        rating={Math.round(5 * primaryAnswer.score)}
-                        maxRating={5}
-                        size="mini"
-                        disabled
-                      />
-                      <div className="answers__bottom__spacer"></div>
-                      <Popup
-                        trigger={
-                          <Button basic size="mini">
-                            Direct answer
-                          </Button>
-                        }
-                      >
-                        <AnswerBoxDetails />
-                      </Popup>
+                      {links.length ? (
+                        <div className="answers__links">
+                          <AnswerLinksList
+                            appConfig={appConfig}
+                            filtered={links}
+                          />
+                        </div>
+                      ) : (
+                        ''
+                      )}
                     </div>
                   </Segment>
                 </div>
