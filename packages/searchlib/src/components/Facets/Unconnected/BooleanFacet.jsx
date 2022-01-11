5da3fa5708d10395332d1e678d675699762bde66
GhitaB <ghitabizau27@gmail.com>
2022-01-11 15:27:46 +0200

    Refs #136090 - Simplified selectors on smaller screen.

--- packages/searchlib/src/components/Facets/Unconnected/BooleanFacet.jsx
+++ packages/searchlib/src/components/Facets/Unconnected/BooleanFacet.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Radio } from 'semantic-ui-react';
+import { useWindowDimensions } from '@eeacms/search/lib/hocs';
 
 const truthy = (val) => {
   if (typeof val === 'string') {
@@ -21,11 +22,18 @@ export const BooleanFacetComponent = (props) => {
   const filter = filters.find((filter) => filter.field === field);
   const value = filter ? truthy(filter.values[0]) : false;
 
+  const { width } = useWindowDimensions();
+  const isSmallScreen = width < 550;
+  let textLabel = label;
+  if (isSmallScreen && label === 'Include archived content') {
+    textLabel = 'Archived'; // TODO: shortLabel as setting in facets config?
+  }
+
   return (
     <div className="boolean-facet" {...domProps} id={id}>
       <Radio
         toggle
-        label={label}
+        label={textLabel}
         checked={value}
         onChange={(e, { checked }) => {
           if (checked) {
