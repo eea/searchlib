6a23668f05007fc036f1e9f004c68c4bb737aa20
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-04-28 15:45:14 +0300

    Delete datahub package, migrated to volto-datahub

--- packages/searchlib-datahub/src/config/filters.js
+++
@@ -1,4 +0,0 @@
-export default {
-  // filter values that are always added to the ES requests
-  permanentFilters: [{ term: { cluster_name: 'sdi' } }],
-};
--- packages/searchlib-datahub/src/config/vocabulary.js
+++
@@ -1,7 +0,0 @@
-const vocab = {
-  cluster_name: {
-    sdi: 'Datahub series'
-  },
-};
-
-export default { vocab };
